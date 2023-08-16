3d914c67461172ce841586fc6d96e60ea1b947cd
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-08-16 09:00:28 -0600

    Add files via upload

---
+++ programa03.py
@@ -0,0 +1,37 @@
+#Tercer metodo para calcular la derivada
+from math import*
+def f(x):
+	return eval(fs)
+def fp(x):
+	return eval(fps)	
+def phi(x,h):
+	return (f(x+h) - f(x-h))/(2*h)
+	
+fs = input('f(x) = ')
+fps = input("f'(x) = ")
+x = eval(input('x = '))
+print ('   i		h		 fp		  error')
+
+#primera itereacion
+h = 4**(-1)
+
+aprox = 4*phi(x,h/2)/3 - phi(x,h)/3
+error = abs(aprox-fp(x))
+print('{:4.0f}{:20.15f}{:20.15f}{:20.15f}'.format(1, h, aprox, error))
+minimo = error
+mejor_aprox = aprox
+valor_indice = 1
+
+#iteraciones restantes
+for i in range(2,33):
+	h = 4**(-i)
+	aprox = 4*phi(x,h/2)/3 - phi(x,h)/3
+	error = abs(aprox-fp(x))
+	print('{:4.0f}{:20.15f}{:20.15f}{:20.15f}'.format(i, h, aprox, error))
+	if error <= minimo:
+		minimo = error
+		mejor_aprox = aprox
+		valor_indice = i
+		
+print("Mejor aproximacion para f' = ", mejor_aprox)
+print("Indice correspondiente = ", valor_indice)
