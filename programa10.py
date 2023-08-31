397069695566f8c12bb4d84b6738ff4ff63a04ec
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-08-31 08:13:36 -0600

    Update programa10.py
    Se modificó el valor de h

--- programa10.py
+++ programa10.py
@@ -8,11 +8,13 @@ a = eval(input('Límmite inferior = '))
 b = eval(input('Límmite superior = '))
 n = eval(input('Número de subintervalos = '))
 
-h = (b - a)/(2**n)
+h = (b - a)
 R = [0]*n
 R[0] = (b-a)*(f(a)+f(b))/2
-for i in range(1,n+2):
+print('{:5.15f}'.format(R[0]))
+for i in range(1,n):
 	suma = 0
+	h = (b-a)/2**i
 	for k in range(1, 2**(i-1)):
 		suma = suma + h*f(a + (2*k - 1)*h)
 	R[i] = R[i-1]/2 + suma
