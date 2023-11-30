1cdf57735d2defd0f37890e8e5925f02261f995b
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-11-29 22:30:21 -0600

    Update programa19.py

--- programa19.py
+++ programa19.py
@@ -3,31 +3,28 @@
 
 from math import*
 
-def x(s):
-	return eval(xs)
+def f(x):
+	return eval(fs)
 	
-def t(s):
+def g(x):
 	return eval(ts)
 	
-def metodo_euler(xh,th):
-	return xh + h*(x(xh) + t(th))
+def metodo_euler(x,t):
+	return x + h*(f(x) + g(t))
 	
-xs = input('x prima, sólo términos x = ')
+fs = input('x prima, sólo términos x = ')
 ts = input('x prima, sólo términos t = ')
 a  = eval(input('Tiempo inicial = '))
 b  = eval(input('Tiempo final = '))
 n  = eval(input('Número de pasos = '))
-x  = eval(input('Valor inicial de x = '))
+val  = eval(input('Valor inicial de x = '))
 print ('   t		x')
-print('{:4.0f}{:20.15f}'.format(a, x))
+print('{:4.4f}{:20.10f}'.format(a, val))
 
-h = (a-b)/n
+h = (b-a)/n
 
 for i in range(1,101):
-	t = i*h
-	xh = metodo_euler(x, a+t)
-	print('{:4.0f}{:20.15f}'.format(a+t, x))
-	x = xh
-
-
-
+	tiempo = a + i*h
+	xh = metodo_euler(val, tiempo)
+	print('{:4.4f}{:20.10f}'.format(tiempo, xh))
+	val = xh
